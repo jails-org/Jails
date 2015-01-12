f8ac9507151af70a76fd905d579d338ce4cad1c1
Eduardo Aragão <edudu_tata@hotmail.com>
2015-01-12 13:02:33 -0200

    Removing backbone assets references

--- todomvc/assets/js/config.js
+++ todomvc/assets/js/config.js
@@ -4,7 +4,7 @@ require.config({
 	deps    :['mustache', 'jails', global.page],
 
 	paths   :{
-		jails		:'/Jails/source/jails.min',
+		jails		:'//rawgit.com/Javiani/Jails/master/source/jails.min',
 		mods		:'//rawgit.com/jails-org/Modules/master',
 		mustache  	:'//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min'
 	},
