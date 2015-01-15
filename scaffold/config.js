d2fb2e9b0d93813c4377a39727912d776af6a608
Eduardo Aragão <edudu_tata@hotmail.com>
2015-01-15 19:22:50 -0200

    Adding config scaffold template

---
+++ scaffold/config.js
@@ -0,0 +1,22 @@
+require.config({
+
+	baseUrl :'assets/js/',
+	deps    :['jquery', 'mustache', 'jails', <%=name%>],
+
+	paths   :{
+
+		jails		:'//rawgit.com/Javiani/Jails/master/source/jails.min',
+		mods		:'//rawgit.com/jails-org/Modules/master',
+		comps		:'//rawgit.com/jails-org/Components/master',
+		jquery 		:'//code.jquery.com/jquery-2.1.1.min',
+		mustache  	:'//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min'
+	},
+
+	callback :function( jquery, mustache, jails ){
+
+		jails.start({
+			base        :jquery,
+			templates   :{ engine :mustache }
+		});
+	}
+});
