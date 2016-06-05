e1946fdf472f452a64473c4d80e041321a203793
Eduardo Ottaviani <Edu@MacBook-Pro.local>
2016-06-05 04:08:05 -0300

    Refactory, adding prototype inheritance, improving speed and adding some useful methods

--- source/jails.js
+++ source/jails.js
@@ -11,65 +11,136 @@
 	}
 }(this, function () {
 
-	var Jails, config, slice,
-		global = {},
-		publisher = PubSub(),
-		root = document.documentElement;
+	var Jails 		= {},
+		global 		= {},
+		publisher 	= pubsub(),
+		slice 		= Array.prototype.slice,
+		root		= document.documentElement;
 
-	slice = Array.prototype.slice;
+	Jails.events = on();
+	Jails.publish = publisher.publish;
+	Jails.subscribe = publisher.subscribe;
 
-	Jails = {
+	Jails.apps = {};
+	Jails.controllers = {};
+	Jails.components = {};
 
-		apps 		:{},
-		controllers	:{},
-		components	:{},
-		events 		:On(),
+	Jails.app = factory( Controller, 'apps' );
+	Jails.component = factory( Component, 'components' );
+	Jails.controller = factory( Controller, 'controllers');
 
-		app 		:_Class('apps', Controller),
-		controller 	:_Class('controllers', Controller),
-		component	:_Class('components', Component),
+	Jails.Controller = Controller;
+	Jails.Component  = Component;
+	Jails.App 		 = Controller;
 
-		publish 	:publisher.publish,
-		subscribe 	:publisher.subscribe,
-
-		start :function( ctx ){
-
-			Scanner.scan( ctx );
-			root.className = root.className.replace('jls-ready', '') + ' jls-ready';
-		},
-
-		refresh :function( ctx ){
-			Scanner.scan( ctx );
-		},
-
-		data :function(){
-			return global;
-		},
-
-		scanner :function(){
-			return Scanner;
-		},
-
-		render :function( container, html ){
-			Scanner.scan(container, function(item){
-				Jails.events.trigger(item, 'execute', 'destroy' );
-			});
-			container.innerHTML = html;
-		}
+	Jails.start = function( ctx ){
+		Jails.scanner.scan( ctx );
+		Jails.publish('jails:ready');
 	};
 
-	var Scanner = {
+	Jails.refresh = function( ctx ){
+		Jails.scanner.scan( ctx );
+	};
 
-		scan :function( context, callback ){
+	Jails.data = function(){
+		return global;
+	};
 
-			var current, list, entities = this.entities, ctx;
-			ctx = context || document;
-			for( var entity in entities ){
-				current = entities[ entity ];
-				list = ctx.querySelectorAll( current.selector );
-				forEach(list, callback || current.method);
+	Jails.render = function( container, html ){
+		Jails.scanner.scan( container, function( element ){
+			Jails.events.trigger( element, 'destroy');
+		});
+		container.innerHTML = html;
+	};
+
+	function Component( name, element ){
+
+		this.name = name;
+		this.element = element;
+		this._events = {};
+
+		Jails.events.on(element, 'execute', execute(this, name));
+		Jails.events.on(element, 'destroy', destroy(this));
+	}
+
+	Component.prototype.trigger = function( ev, target, args ){
+		if( target && target.constructor == String ){
+			forEach(this.element.querySelectorAll(target), function(children){
+				Jails.events.trigger(children, ev, args );
+			});
+		}else{
+			Jails.events.trigger( this.element, ev, args );
+		}
+	}
+
+	Component.prototype.emit = function( simbol, args ){
+		Jails.events.trigger( this.element, this.name+':'+simbol, args );
+	}
+
+	Component.prototype.on = function( ev, query, method ){
+
+		if(!method){
+			method = query;
+			query = null;
+		}
+
+		var element = this.element;
+		var events = this._events;
+
+		events[method] = function(e){
+			if(!query) return method.call(element, e);
+			else{
+				var target = e.target;
+				while( target && target != element && target.parentNode ){
+					if ( target.matches? target.matches(query) :matchesSelector(target, query) ){
+						method.call(target, e);
+						break;
+					}
+					target = target.parentNode;
+				}
 			}
-		},
+		};
+
+		Jails.events.on( element, ev, events[method] );
+		//Off
+		return function(){
+			Jails.events.off( element, ev, events[method] );
+			delete events[method];
+		};
+	}
+
+	function Controller( name, element ){
+		Component.apply(this, arguments);
+	}
+
+	Controller.prototype = Component.prototype;
+
+	Controller.prototype.publish = publisher.publish;
+	Controller.prototype.subscribe = publisher.subscribe;
+
+	Controller.prototype.listen = function( ev, method ){
+		Jails.events.on( this.element, ev, function(e){
+			method.call(e.target, e, e.detail);
+		});
+	};
+
+	Controller.prototype.x = function( target ){
+		var element = this.element;
+		return function(){
+			var args = slice.call( arguments );
+			forEach(element.querySelectorAll(target), function(children){
+				Jails.events.trigger(children, 'execute', args );
+			});
+		};
+	}
+
+	Controller.prototype.get = function( type, name ){
+		if( name )
+			return this.x('[data-'+type+'*='+name+']');
+		return this.x(type);
+	}
+
+	Jails.scanner = {
 
 		entities :{
 
@@ -80,40 +151,62 @@
 
 			controllers:{
 				selector:'[data-controller]',
-				method 	:controller('controller')
+				method 	:module('controller')
 			},
 
 			app :{
 				selector :'[data-app]',
-				method	 :controller('app')
+				method	 :module('app')
+			}
+		},
+
+		scan :function( context, callback ){
+
+			var current, list, entities = this.entities, ctx;
+			ctx = context || document;
+			for( var entity in entities ){
+				current = entities[ entity ];
+				list = ctx.querySelectorAll( current.selector );
+				forEach(list, callback || current.method);
 			}
 		}
 	};
 
-	function create( T, Class, name, element, data ){
-
-		var instantiated = element['jails-'+name];
-		if( instantiated ) return;
-
-		element['jails-'+name] = true;
-
-		var object = new T( name, element );
-		if( Class ) Class.apply( object, [element, data] );
-		if( object.init ) object.init();
+	function module( type ){
+		return function( element ){
+			var name = element.getAttribute('data-'+type), instance;
+			if( instantiated(element, name, type) )
+				return;
+			instance = new Jails[type+'s'][name]( element, global );
+			if( instance.init )
+				instance.init();
+		};
 	}
 
 	function component( element ){
-
-		var anno = annotations( element ),
-		names = element.getAttribute('data-component').split(/\s/);
+		var instance,
+			anno = annotations( element ),
+			names = element.getAttribute('data-component').split(/\s/);
 
 		forEach(names, init);
 
-		function init( item ){
-			if( item in Jails.components ){
-				create( Component, Jails.components[ item ], item, element, anno[item] || {} );
+		function init( name ){
+			if( name in Jails.components ){
+				if( instantiated( element, name, 'component') )
+					return;
+				instance = new Jails.components[name](element, anno[name] || {});
+				if( instance.init )
+					instance.init();
 			}
 		}
+	}
+
+	function instantiated( element, name, type ){
+		var setted = element['jails#'+type+'#'+name];
+		if( setted ) return true;
+
+		element['jails#'+type+'#'+name] = true;
+		return false;
 	}
 
 	function annotations( el ){
@@ -121,7 +214,7 @@
 		var ann = {}, comment, code;
 
 		comment = el.previousSibling;
-		comment = comment && comment.nodeType == 8? comment :comment?comment.previousSibling:null;
+		comment = comment && comment.nodeType == 8? comment :comment? comment.previousSibling : null;
 
 		if(comment && comment.nodeType == 8){
 			code = comment.data
@@ -134,56 +227,9 @@
 		return ann;
 	}
 
-	function controller( T ){
-		return function( element ){
-			var name, object;
-			name = element.getAttribute( 'data-' + T );
-			create( Controller, Jails[ T + 's' ][name], name, element, global);
-		};
-	}
+	function execute( instance, name ){
 
-	function Common(name, element){
-
-		var instance = this, events = {};
-
-		this.name = name;
-		this.trigger = Jails.events.trigger;
-
-		Jails.events.on(element, 'execute', execute);
-
-		this.off = function(ev, fn){
-			Jails.events.off(element, ev, events[fn]);
-		};
-
-		this.emit = function( simbol, args ){
-			Jails.events.trigger( element, name+':'+simbol, args );
-		};
-
-		this.on = function(ev, query, method){
-
-			if(!method){
-				method = query;
-				query = null;
-			}
-
-			events[method] = function(e){
-				if(!query) return method.call(element, e);
-				else{
-					var target = e.target;
-					while( target && target != element && target.parentNode ){
-						if ( target.matches? target.matches(query) :matchesSelector(target, query) ){
-							method.call(target, e);
-							break;
-						}
-						target = target.parentNode;
-					}
-				}
-			};
-
-			Jails.events.on(element, ev, events[method]);
-		};
-
-		function execute(e){
+		return function(e){
 
 			if(e.detail){
 				var d, n, namespace, method;
@@ -200,49 +246,33 @@
 			}
 
 			e.stopPropagation();
-		}
-	}
-
-	function Component(name, element){
-		Common.apply(this, arguments);
-	}
-
-	function Controller( name, element ){
-
-		Common.apply( this, arguments );
-
-		this.publish = publisher.publish;
-		this.subscribe = publisher.subscribe;
-
-		this.listen = function( ev, method ){
-			Jails.events.on( element, ev, function(e){
-				method.call(e.target, e, e.detail);
-			});
-		};
-
-		this.x = function(target){
-			return function(){
-				var args = slice.call( arguments );
-				forEach(element.querySelectorAll(target), function(children){
-					Jails.events.trigger(children, 'execute', args );
-				});
-			};
 		};
 	}
 
-	function _Class( t, T ){
-		return function( name, Subclass ){
-			Jails[ t ][ name ] = Subclass;
-			return function( element, data ){
-				T.apply( this, [name, element] );
-				Subclass.apply( this, [element, data] );
-			};
+	function destroy( instance ){
+		return function(){
+			for(var i in instance._events)
+				Jails.events.off(instance.element, instance._events[i]);
+			for( i in instance )
+				instance[i] = null;
+			instance = null;
+		};
+	}
+
+	function factory( C, prop ){
+		return function( name, Mixin ){
+			function Class( html, data ){
+				C.call(this, name, html);
+				Mixin.call(this, html, data);
+			}
+			Class.prototype = C.prototype;
+			Jails[ prop ][ name ] = Class;
 		};
 	}
 
 	//Inspired by:
 	//http://dev.housetrip.com/2014/09/15/decoupling-javascript-apps-using-pub-sub-pattern/
-	function PubSub(){
+	function pubsub(){
 
 		var topics = {};
 		var _async = {};
@@ -262,6 +292,23 @@
 					topics[key] = topics[key] || [];
 					topics[key].push( method );
 				}
+				//Unsubscribe
+				return function(){
+					var newtopics = [];
+					var newasync  = [];
+
+					forEach(topics[key], function( fn){
+						if(fn != method)
+							newarr.push(fn);
+					});
+					topics[key] = newtopics;
+
+					forEach(_async[key], function( fn){
+						if(fn != method)
+							newarr.push(fn);
+					});
+					_async[key] = newasync;
+				};
 			},
 
 			publish :function(){
@@ -282,36 +329,9 @@
 		};
 	}
 
-	function forEach(array, fn){
-		for(var i = 0, len = array.length; i < len; i++)
-			fn(array[i], i);
-	}
-
-	function dup(o){
-		var f = function(){};
-		f.prototype = o;
-		return new f();
-	}
-
-	// http://tanalin.com/en/blog/2012/12/matches-selector-ie8/
-	function matchesSelector(elem, selector) {
-
-		var elems = elem.parentNode.querySelectorAll(selector),
-			count = elems.length;
-
-		for (var i = 0; i < count; i++) {
-			if (elems[i] === elem) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	function On(){
+	function on(){
 
 		//http://gist.github.com/jonathantneal/7366668
-		var hasfocusin = true;
 		var CustomEv = (function(){
 			try {
 				var p = new CustomEvent('c', { detail: { foo: 'b' } });
@@ -337,10 +357,7 @@
 		return {
 
 			on :(function(){
-				return hasfocusin? function(el, e, fn){
-					var newe = e == 'focus'? 'focusin' :e == 'blur'? 'focusout':e;
-					el.addEventListener(newe, fn, false);
-				}:function(el, e, fn){
+				return function(el, e, fn){
 					el.addEventListener(e, fn, (e == 'focus' || e == 'blur'));
 				};
 			})(),
@@ -357,6 +374,33 @@
 				}
 			}
 		};
+	}
+
+	function forEach(array, fn){
+
+		for(var i = 0, len = array? array.length :0; i < len; i++)
+			fn(array[i], i);
+	}
+
+	function dup(o){
+		var f = function(){};
+		f.prototype = o;
+		return new f();
+	}
+
+	// http://tanalin.com/en/blog/2012/12/matches-selector-ie8/
+	function matchesSelector(elem, selector) {
+
+		var elems = elem.parentNode.querySelectorAll(selector),
+			count = elems.length;
+
+		for (var i = 0; i < count; i++) {
+			if (elems[i] === elem) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	return Jails;
