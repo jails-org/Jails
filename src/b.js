6476eab134d861dd3b8aa246a9ce1ecb99902432
Javiani <eduardo.ottaviani@gmail.com>
2019-08-03 19:35:06 -0300

    Trying again from scratch

---
+++ src/b.js
@@ -0,0 +1,19 @@
+export default () => {
+
+    console.log('B')
+
+}
+
+export const model = {
+    name: 'name = B'
+}
+
+export const actions = {
+
+}
+
+
+export const view = (state) => {
+
+    return state
+}
---
+++ src/c.js
@@ -0,0 +1,18 @@
+export default () => {
+
+    console.log('C')
+}
+
+export const model = {
+    name: 'name = C'
+}
+
+export const actions = {
+
+}
+
+
+export const view = (state) => {
+
+    return state
+}
