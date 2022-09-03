7ee44a5ed95d4212f5167d89fabdef56f5efef5e
Javiani <edudu_tata@hotmail.com>
2022-09-03 02:20:32 -0300

    Updating with templateConfig feature

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 declare const _default: {
     register(name: string, module: any, dependencies?: object): void;
     start(): void;
-	templateConfig( options: any )
+	templateConfig( options: any ): void;
 };
 export default _default;
